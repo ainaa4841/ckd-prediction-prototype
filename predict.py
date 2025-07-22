f722f93d5e26adcb31996780bfdcc0951a0f5500
ainaa4841 <ainamhmdd@gmail.com>
2025-07-23 02:33:12 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,11 +1,17 @@
 import pickle
 import numpy as np
+import joblib
 
 def load_model():
     with open("model/xgb_model.pkl", "rb") as file:
         model = pickle.load(file)
     return model
 
-def make_prediction(model, input_array):
-    prediction = model.predict(input_array)
+def load_scaler():
+    scaler = joblib.load("model/scaler.pkl")
+    return scaler
+
+def make_prediction(model, scaler, input_array):
+    scaled_input = scaler.transform(input_array)
+    prediction = model.predict(scaled_input)
     return int(prediction[0])
